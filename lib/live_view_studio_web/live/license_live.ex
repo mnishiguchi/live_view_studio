2ef03577101f02ab010ebb822366658b6f79cf29
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-09-12 17:28:31 -0400

    Add countdown timer to LicenseLive

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -3,11 +3,23 @@ defmodule LiveViewStudioWeb.LicenseLive do
 
   alias LiveViewStudio.Licenses
   import Number.Currency
-  import Inflex
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, seats: 3, amount: Licenses.calculate(3))
-    {:ok, socket}
+    if connected?(socket) do
+      # Count down every second.
+      :timer.send_interval(1000, self(), :tick)
+    end
+
+    one_hour_from_now = Timex.shift(Timex.now(), hours: 1)
+
+    initial_state = %{
+      seats: 3,
+      amount: Licenses.calculate(3),
+      expiration_time: one_hour_from_now,
+      time_remaining: time_remaining(one_hour_from_now)
+    }
+
+    {:ok, assign(socket, initial_state)}
   end
 
   def render(assigns) do
@@ -33,7 +45,16 @@ defmodule LiveViewStudioWeb.LicenseLive do
           <div class="amount">
             <%= number_to_currency(@amount) %>
           </div>
+
+          <p class="m-4 font-semibold text-indigo-800">
+            <%= if @time_remaining > 0 do %>
+              <%= format_time(@time_remaining) %> left to save 20%
+            <% else %>
+              Expired!
+            <% end %>
+          </p>
         </div>
+
       </div>
     </div>
     """
@@ -53,5 +74,41 @@ defmodule LiveViewStudioWeb.LicenseLive do
       )
 
     {:noreply, socket}
+  end
+
+  @doc """
+  Called every second. Updates the time remaining.
+  """
+  def handle_info(:tick, socket) do
+    expiration_time = socket.assigns.expiration_time
+    socket = assign(socket, time_remaining: time_remaining(expiration_time))
+    {:noreply, socket}
+  end
+
+  # Returns time remaining in seconds.
+  #
+  # ## Examples
+  #
+  #     iex> expiration_time = Timex.shift(now, minutes: 30)
+  #     ~U[2020-09-12 21:36:57.142111Z]
+  #
+  #     iex> time_remaining(expiration_time)
+  #     1347
+  #
+  defp time_remaining(expiration_time) do
+    DateTime.diff(expiration_time, Timex.now())
+  end
+
+  # Converts seconds to humanized time string.
+  #
+  # ## Examples
+  #
+  #     iex> format_time(1347)
+  #     "22 minutes, 27 seconds"
+  #
+  defp format_time(seconds) do
+    seconds
+    |> Timex.Duration.from_seconds()
+    |> Timex.format_duration(:humanized)
   end
 end
